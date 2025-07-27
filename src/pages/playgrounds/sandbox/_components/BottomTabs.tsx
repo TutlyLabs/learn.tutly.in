2f295044931569dd447d79a5aebe32a419558efa
Uday <udaysagar.mail@gmail.com>
2025-07-28 01:33:22 +0530

    style: minor fix

--- src/pages/playgrounds/sandbox/_components/BottomTabs.tsx
+++ src/pages/playgrounds/sandbox/_components/BottomTabs.tsx
@@ -25,7 +25,7 @@ export function BottomTabs() {
   };
 
   return (
-    <div className="flex flex-col h-[95%]">
+    <div className="flex flex-col h-[93%]">
       {/* Tab Headers */}
       <div
         className="h-[42px] backdrop-blur-xl border-b flex items-center justify-between px-4 flex-shrink-0"
